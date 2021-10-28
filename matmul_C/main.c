2d0d23bf1c28aa785ae1e9f13d50c1d7d7809c6e
Evgeniy.Kishov <kb7c2@rambler.ru>
2021-10-28 21:01:49 +0400

    C Array vs Struct

--- matmul_C/main.c
+++ matmul_C/main.c
@@ -7,19 +7,69 @@
 
 #define USE_PARALLEL 1
 #define SEQUENTIAL   0
- 
-static void matmul2(struct matr* c, const struct matr* a, const struct matr* b, int parl)
+
+struct matr2 {		
+	double** v;	
+	int n, m;
+};
+
+struct matr2* create_matr2(int n, int m)
+{
+	struct matr2* a = NULL;
+	a = calloc(1, sizeof(struct matr2));
+	if (a == NULL)
+		return NULL;
+	a->n = n;
+	a->m = n;
+	a->v = calloc(n, sizeof(double*));
+	if (a->v == NULL)
+		return NULL;
+	for (int i = 0; i < m; i++)
+		a->v[i] = calloc(n, sizeof(double));
+	return a;
+}
+
+double** create_matr_arr(int n)
+{
+	double **a = NULL;
+	a = calloc(n, sizeof(double*));
+	for (int i = 0; i < n; i++)
+		a[i] = calloc(n, sizeof(double));
+	return a;
+}
+
+static void matmul2(struct matr2* c, const struct matr2* a, const struct matr2* b, int parl)
 {
 	int i = 0;
+	//storing pointers gives a performance gain
+	double **cv = c->v,
+	       **av = a->v,
+	       **bv = b->v;
 	#pragma omp parallel for if (parl == USE_PARALLEL)
 	for (i = 0; i < c->n; i++) {
 		int j = 0;
 		//#pragma omp parallel for if (parl == USE_PARALLEL)
 		for (j = 0; j < c->n; j++)
 		{
-			c->v[i][j] = 0.0;
+			cv[i][j] = 0.0;
 			for (int k = 0; k < a->n; k++)
-				c->v[i][j] += a->v[i][k] * b->v[k][j];
+				cv[i][j] += av[i][k] * bv[k][j];
+		}
+	}
+}
+
+static void matmul2_arr(double **c, double **a, double **b, int n, int parl)
+{
+	int i = 0;
+	#pragma omp parallel for if (parl == USE_PARALLEL)
+	for (i = 0; i < n; i++) {
+		int j = 0;
+		//#pragma omp parallel for if (parl == USE_PARALLEL)
+		for (j = 0; j < n; j++)
+		{
+			c[i][j] = 0.0;
+			for (int k = 0; k < n; k++)
+				c[i][j] += a[i][k] * b[k][j];
 		}
 	}
 }
@@ -29,7 +79,7 @@ double rnd(double scale)
 	return ((double)rand()/RAND_MAX)*scale;
 }
 
-void rnd_fill_matr(struct matr *m)
+void rnd_fill_matr(struct matr2 *m)
 {	
 	for (int i = 0; i < m->n; i++)
 		for (int j = 0; j < m->n; j++)
@@ -37,6 +87,16 @@ void rnd_fill_matr(struct matr *m)
 				m->v[i][j] = rnd(100.0);
 			else
 				m->v[i][j] = rnd(10.0);
+}
+
+void rnd_fill_matr_arr(double **m, int n)
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (i == j)
+				m[i][j] = rnd(100.0);
+			else
+				m[i][j] = rnd(10.0);
 }
 
 void rnd_fill_vec(struct vec *v)
@@ -49,20 +109,26 @@ void main()
 {
 	srand((unsigned int)time(0));
 
-	int n = 2000;
-	struct matr *a = create_matr(n, n);
+	int n = 1000;
+	struct matr2 *a = create_matr2(n, n);
+	//double **a = create_matr_arr(n);
 	rnd_fill_matr(a);
+	//rnd_fill_matr_arr(a, n);
 	//printf("Matrix [A]: \n");
 	//print_matr(a, "%6.2f");
 
-	struct matr *b = create_matr(n, n);
+	struct matr2 *b = create_matr2(n, n);
+	//double **b = create_matr_arr(n);
+	//rnd_fill_matr_arr(b, n);
 	rnd_fill_matr(b);
 	//printf("Matrix [B]: \n");
 	//print_matr(b, "%6.2f");
 
-	struct matr *c = create_matr(n, n);
+	struct matr2 *c = create_matr2(n, n);
+	//double **c = create_matr_arr(n);
 	clock_t begin = clock();
-	matmul2(c, a, b, USE_PARALLEL);
+	//matmul2_arr(c, a, b, n, USE_PARALLEL);
+	matmul2(c, a, b, SEQUENTIAL);
 	clock_t end = clock();
 	double time_spent = (double)(end - begin);
 	printf("time spent = %lf\n", time_spent);
@@ -70,8 +136,8 @@ void main()
 	//printf("Matrix [C] = [A]*[B]: \n");
 	//print_matr(c, "%12.2f");
 
-	delete_matr(&a);
-	delete_matr(&b);
-	delete_matr(&c);
+	//delete_matr(&a);
+	//delete_matr(&b);
+	//delete_matr(&c);
 	getchar();
 }
